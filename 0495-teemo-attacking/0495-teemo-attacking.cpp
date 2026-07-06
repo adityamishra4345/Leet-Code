ac62f315acaa765e4ddd3f00bcb5d4dd2beac560
Aditya Mishra <adityamishra4345@gmail.com>
2026-07-06 12:29:09 +0530

    Time: 0 ms (100%), Space: 29.7 MB (62.21%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& ts, int d) {
-        int n=ts.size();
         int c=0;
-        int overlap=0;
          int att=0;
-        for(int i=0;i<n;i++){
-            
-            overlap=0;
+        for(int i=0;i<ts.size();i++){
+            int overlap=0;
             if(i>0 && ts[i]<=att){
                  overlap=att-ts[i]+1;    
              }
              att=ts[i]+d-1;
             c+=d-overlap;
-
         }
         return c;
     }
