60d831f0eb355b640c704cad098e8c228aecfb3c
Aditya Mishra <adityamishra4345@gmail.com>
2026-07-08 17:42:45 +0530

    Time: 45 ms (50.26%), Space: 139.9 MB (93.26%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& q) {
         int n = s.size();
-        const long long MOD = 1e9 + 7;
+        const int MOD = 1e9 + 7;
 
-        vector<long long> sumPre(n + 1, 0);
-        vector<long long> numPre(n + 1, 0);
-        vector<long long> cntPre(n + 1, 0);
-        vector<long long> pw(n + 1, 1);
+        vector<int> sumPre(n + 1, 0);
+        vector<int> numPre(n + 1, 0);
+        vector<int> cntPre(n + 1, 0);
+        vector<int> pw(n + 1, 1);
 
         for (int i = 1; i <= n; i++)
-            pw[i] = pw[i-1] * 10 % MOD;
+            pw[i] = (long long)pw[i-1] * 10 % MOD;
 
         for (int i = 0; i < n; i++) {
             int d = s[i] - '0';
             sumPre[i+1] = sumPre[i] + d;
             cntPre[i+1] = cntPre[i] + (d != 0);
-            numPre[i+1] = (d != 0) ? (numPre[i] * 10 + d) % MOD : numPre[i];
+            numPre[i+1] = d ? ((long long)numPre[i] * 10 + d) % MOD : numPre[i];
         }
 
         vector<int> ans;
+        ans.reserve(q.size());
+
         for (auto& query : q) {
             int a = query[0], b = query[1];
-            long long digitSum = sumPre[b+1] - sumPre[a];
-            long long nonZeroCnt = cntPre[b+1] - cntPre[a];
-            long long num = (numPre[b+1] - numPre[a] * pw[nonZeroCnt] % MOD + MOD) % MOD;
-            ans.push_back(num * digitSum % MOD);
+            int digitSum = sumPre[b+1] - sumPre[a];
+            int nonZeroCnt = cntPre[b+1] - cntPre[a];
+            int num = ((long long)numPre[b+1] - (long long)numPre[a] * pw[nonZeroCnt] % MOD + MOD) % MOD;
+            ans.push_back((long long)num * digitSum % MOD);
         }
         return ans;
     }
